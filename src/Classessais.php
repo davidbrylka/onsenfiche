74dc7d9a8f89d83db8d48a34d810fbf1532212d8
davidbrylka <gt-turbo@jagware.org>
2021-10-13 10:22:59 +0200

    On va y arriver

---
+++ src/Classessais.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Moi\TddEssais;
+
+class Essais
+{
+
+    public function Methode1()
+    {
+
+    }
+    
+}
+
+?>
---
+++ tests/TestClassessais.php
@@ -0,0 +1,16 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+
+class Essais extends TestCase{
+
+    public function testMethode1()
+    {
+
+    }
+
+}
+
+
+
+?>
